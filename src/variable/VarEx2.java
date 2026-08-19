a73017f741c726f60b22abb5cf34df13c521d208
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-08-19 22:25:18 +0900

    자바 변수 및 기본 입출력 실습

---
+++ src/variable/VarEx2.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarEx2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;   // 다썼으면 땀을 꼭 흘려주자
+
+        System.out.println(sum);
+    }
+}
