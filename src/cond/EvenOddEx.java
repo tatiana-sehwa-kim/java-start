a2d04a1ea4fd1779183b6ad9af337c0126a1ec6c
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-08-21 18:35:20 +0900

    자바 조건문

---
+++ src/cond/EvenOddEx.java
@@ -0,0 +1,11 @@
+package cond;
+
+public class EvenOddEx {
+    public static void main(String[] args) {
+
+        int x = 2;
+        String evenodd = (x%2 == 0) ? "짝수" : "홀수";
+
+        System.out.println(evenodd);
+        }
+    }
