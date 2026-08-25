cf7af5e77f91c6dfab510303c3db95243f69cb69
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-08-25 22:30:51 +0900

    자바 배열과 리팩토링

---
+++ src/array/ArrayEx1.java
@@ -0,0 +1,23 @@
+package array;
+
+public class ArrayEx1 {
+    public static void main(String[] args) {
+
+        int students[] = {90, 80, 70, 60, 50};
+
+        int total = 0;
+
+        for ( int i = 0; i < students.length; i++ ){
+            total += students[i];
+        }
+        double average = total/students.length;
+
+        System.out.println("점수 총합: " + total);
+        System.out.println("점수 평균: " + average);
+    }
+}
+
+//향상된 for문을 쓰는게 더깔끔하긴 하다.
+// for (int student : students){
+//  total += student;
+// }
