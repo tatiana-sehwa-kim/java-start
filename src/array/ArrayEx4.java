a4681c9f4fe23dd91b1da5f97910783446f74587
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-08-26 22:26:28 +0900

    자바 배열과 리팩토링 예제

---
+++ src/array/ArrayEx4.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayEx4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        double average;
+        int count;
+
+        System.out.print("입력받을 숫자의 개수를 입력하세요:");
+        count= scanner.nextInt();
+        int[] numbers = new int[count];
+
+
+        System.out.println(count + "개의 정수를 입력하세요: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+        }
+        average = (double) sum / numbers.length;
+
+        System.out.println("입력한 정수의 합계: " + sum);
+        System.out.println("입력한 정수의 평균: " + average);
+
+    }
+}
